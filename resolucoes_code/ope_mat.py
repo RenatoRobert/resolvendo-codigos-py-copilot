7e2ecfd4053881bf7e8afc282b331e6f855fb872
RenatoRobert <164108253+RenatoRobert@users.noreply.github.com>
2024-05-25 17:33:17 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,9 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+string_input = input("Digite uma string: ")
+
+numero_input = int(input("Digite um número inteiro: "))
+
+resultado = string_input * numero_input
+
+print("Resultado: " + resultado)
